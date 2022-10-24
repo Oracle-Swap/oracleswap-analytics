7856d960a63d7d2dc7ce4cae06e2d67da6021173
top1st <riguang102@gmail.com>
2022-10-24 10:32:58 +0800

    Update queries.js

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "oracle/exchnage") {
+    indexingStatusForCurrentVersion(subgraphName: "oracle/exchange") {
       synced
       health
       chains {
